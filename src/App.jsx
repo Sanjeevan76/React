98086bd96104fe2c4239c58b0d0a4b613a70aee0
Sanjeevan76 <studyyou40@gmail.com>
2025-08-02 19:36:57 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,94 @@
 
-import './App.css';
-import { useState } from 'react';
-import Toggle from './toggle.jsx';
+// import './App.css';
+// import { useState } from 'react';
+// import Toggle from './toggle.jsx';
+// import MultToggle from './multToggle.jsx';
+// import Props1 from './props1.jsx';
 
-function App() {
-  const[fruit,setFruit]=useState(0);
-  const handleName=()=>{
-    setFruit(fruit+1);
-  }
+// function App() {
+//   const[fruit,setFruit]=useState(0);
+//   const handleName=()=>{
+//     setFruit(fruit+1);
+//   }
 
-  const[love,setLove]=useState("Hiii");
+//   const[love,setLove]=useState("Hiii");
 
-  // const handleLove = () => {
-  //   setLove(love === "love you" ? "Love You More" : "love you");
-  // };
+//   // const handleLove = () => {
+//   //   setLove(love === "love you" ? "Love You More" : "love you");
+//   // };
   
+//   return (
+//     <>
+//     <Toggle/>
+//      <h1>Hello Everyone</h1>
+//      <h2>{fruit}</h2>
+//      <button onClick={handleName}>
+//     Click Me
+//      </button>
+  
+//     <h2>{love}</h2>
+//      <button onClick={()=>setLove(love === "love you" ? "Love You More" : "love you")}>
+//      Click Me
+//       </button>
+//       {/* handle onclick event without fn */}
+//       <br></br>
+//       <br></br>
+//     <MultToggle/>
+
+//       {/* Props */}
+//       <Props1 name={"sanjeevan"} age={20} email="sam@gmail.com"/>
+//      </>
+//   )
+// }
+
+// export default App
+
+
+//props ,in above code one props send,object send in this code
+// import React from 'react'
+// import Props1 from './props1';
+
+// const App = () => {
+//   let userOBJ={
+//       name:"sanjeevan",
+//       age:20,
+//       email:"sam@gmail.com"
+//   }
+//   let userOBJ1={
+//       name:"sam",
+//       age:20,
+//       email:"sam@gmail.com"
+//   }
+//   let userOBJ2={
+//       name:"sanjeevan kumar",
+//       age:20,
+//       email:"sam@gmail.com"
+//   }
+  
+//   return (
+//     <div>
+//       <Props1 user={userOBJ}/>
+//       <Props1 user={userOBJ1}/>
+//       <Props1 user={userOBJ2}/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//props ARRAY
+
+import React from 'react';
+import Props1 from './props1';
+
+const App = () => {
+  let array=["sam","elen","mark"];
   return (
-    <>
-    <Toggle/>
-     <h1>Hello Everyone</h1>
-     <h2>{fruit}</h2>
-     <button onClick={handleName}>
-    Click Me
-     </button>
-  
-    <h2>{love}</h2>
-     <button onClick={()=>setLove(love === "love you" ? "Love You More" : "love you")}>
-     Click Me
-      </button>
-      {/* handle onclick event without fn */}
-      
-    
-     </>
+    <div>
+ <Props1 props={array}/>
+    </div>
   )
 }
 
 export default App
+
